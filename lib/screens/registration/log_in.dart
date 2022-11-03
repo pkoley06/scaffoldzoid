141da0a4cc5ac1f6b97227b6de7fdf538ee99281
pkoley06 <pkoley06@gmail.com>
2022-11-03 21:10:59 +0530

    sign up

--- lib/screens/registration/log_in.dart
+++ lib/screens/registration/log_in.dart
@@ -10,7 +10,6 @@ class LogInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 0,
         systemOverlayStyle: null,
@@ -25,125 +24,132 @@ class LogInScreen extends StatelessWidget {
               color: AppColor.cyan,
             )),
       ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-                child: Column(
+      body: Center(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Column(
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    const Text(
-                      "Login",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    Column(
+                      children: <Widget>[
+                        const Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Lgin to your account',
+                          style: TextStyle(fontSize: 15, color: AppColor.grey),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(
+                      height: 40,
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: <Widget>[
+                          textButton(label: "Email"),
+                          textButton(label: "Password", obsecureText: true)
+                        ],
+                      ),
+                    ),
+
+                    // Buyer login
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 1, left: 1),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: MaterialButton(
+                            minWidth: double.infinity,
+                            height: 50,
+                            color: AppColor.purple,
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            onPressed: () {},
+                            child: const Text(
+                              'Buyer Login',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            )),
+                      ),
                     ),
                     const SizedBox(
                       height: 20,
                     ),
-                    Text(
-                      'Lgin to your account',
-                      style: TextStyle(fontSize: 15, color: AppColor.grey),
+
+                    // Seller login
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 1, left: 1),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: MaterialButton(
+                            minWidth: double.infinity,
+                            height: 50,
+                            color: AppColor.cyan,
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                            onPressed: () {},
+                            child: const Text(
+                              'Seller Login',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            )),
+                      ),
                     ),
-                  ],
-                ),
-
-                const SizedBox(
-                  height: 40,
-                ),
-
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
-                    children: <Widget>[
-                      textButton(label: "Email"),
-                      textButton(label: "Password", obsecureText: true)
-                    ],
-                  ),
-                ),
-
-                // Buyer login
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 1, left: 1),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
+                    const SizedBox(
+                      height: 20,
                     ),
-                    child: MaterialButton(
-                        minWidth: double.infinity,
-                        height: 50,
-                        color: AppColor.purple,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        onPressed: () {},
-                        child: const Text(
-                          'Buyer Login',
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                // Seller login
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 1, left: 1),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Text("Don't have an acount?"),
+                        Text(
+                          " Sign up",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        )
+                      ],
                     ),
-                    child: MaterialButton(
-                        minWidth: double.infinity,
-                        height: 50,
-                        color: AppColor.cyan,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        onPressed: () {},
-                        child: const Text(
-                          'Seller Login',
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Text("Don't have an acount?"),
-                    Text(
-                      " Sign up",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+
+                    // logo
+                    Container(
+                      padding: const EdgeInsets.only(top: 50),
+                      height: 100,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('asset/juice.png'),
+                              fit: BoxFit.fitHeight)),
                     )
                   ],
-                ),
-
-                // logo
-                Container(
-                  padding: const EdgeInsets.only(top: 50),
-                  height: 100,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('asset/juice.png'),
-                          fit: BoxFit.fitHeight)),
-                )
+                ))
               ],
-            ))
-          ],
+            ),
+          ),
         ),
       ),
     );
--- lib/screens/registration/sign_up.dart
+++ lib/screens/registration/sign_up.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scaffoldzoid/utils/const/app_colors.dart';
 import 'package:scaffoldzoid/utils/widgets/navigator.dart';
+import 'package:scaffoldzoid/utils/widgets/text_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -9,7 +10,6 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 0,
         systemOverlayStyle: null,
@@ -23,6 +23,44 @@ class SignUpScreen extends StatelessWidget {
               size: 20,
               color: AppColor.cyan,
             )),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          height: MediaQuery.of(context).size.height - 50,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    const Text(
+                      "Register",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Sign Up to your account',
+                      style: TextStyle(fontSize: 15, color: AppColor.grey),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    textButton(label: "Name"),
+                    textButton(label: "Email"),
+                    textButton(label: "Password", obsecureText: true),
+                    textButton(label: "Confirm password", obsecureText: true),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
